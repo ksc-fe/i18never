2e4de3fa1d95ee942466e9b7efdea6c2836bbd84
zhangbing4 <zhangbing4@kingsoft.com>
2023-06-09 15:46:17 +0800

    feat: add plugin-webpack

---
+++ packages/plugin-webpack/README.md
@@ -0,0 +1,11 @@
+# `i18never-webpack-plugin`
+
+> TODO: description
+
+## Usage
+
+```
+const i18neverWebpackPlugin = require('i18never-webpack-plugin');
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/plugin-webpack/__tests__/assets/test.vue
@@ -0,0 +1,21 @@
+<script>
+    // import TestComponent from "./testComponent.vue";
+    import config from "./test"
+    export default{
+        data() {
+            return {
+                test: config.a
+            }
+        },
+        components: {
+            TestComponent
+        },
+    }
+</script>
+
+<template>
+    <div>{{ test }}</div>
+    <div>{{ `[$_:jp=n,en=n,ko=n]购买${a}台` }}</div>
+    <div>购买{{ a }}台</div>
+    <TestComponent :a="`[$_:jp=n,en,ko]购买${a}台`" :b="{a: '[$_:jp,en,ko]购买'}" />
+</template>
